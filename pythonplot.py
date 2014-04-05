43bd7b1d3f8856327cbc0f2251ae20fc4dbccc28
Jonne <J.S.Moeskops@student.tudelft.nl>
2014-04-05 16:33:14 +0200

    pythonplot works very nicely, and latex upgrades

--- pythonplot.py
+++ pythonplot.py
@@ -1,5 +1,6 @@
 from __future__ import division, print_function
 from matplotlib import pyplot
+from scipy.optimize import curve_fit
 import numpy as np
 
 filedata=np.genfromtxt("polymerdata.txt")
@@ -28,8 +29,29 @@ for l in lengths:
 	datasize[i]=len(lengthdata[:,1])
 	del lengthdata
 	i=i+1
-print(datasize)
-theo=(lengths-1)**(1.5)
+
+
+# define function to fit the end to end distance
+def fitfunc(x,a):
+	return a*(x-1)**(1.5)
+
+# define function to fit the gyradius
+def fitfunc2(x,a,b):
+	return a*(x-1)**(b)
+
+# fit the data to the theoretical function for end to end distance
+a1,b1=curve_fit(fitfunc,lengths,avrEtoE)
+theo=fitfunc(lengths,a1)
+
+
+# Determinde the fitted function for gyradius
+a2,b2=curve_fit(fitfunc2,lengths,avrGyradius)
+
+theo2=fitfunc2(lengths,a2[0],a2[1])
+
+print("Fitconstant EtoE:",a1)
+print("Fitconstants Gyradius:",a2)
+
 
 fig = pyplot.figure("Polymerplot")
 ax = fig.add_subplot(111)
@@ -41,15 +63,22 @@ ax.set_yscale('log')
 
 ax.errorbar(lengths, avrEtoE,yerr=errEtoE**(1/2),fmt='o', label="End to End distance")
 ax.plot(lengths, theo, 'r--', label="Theoretical End to End")
+
 ax.plot(lengths, datasize, 'go', label="Datasize")
-ax.legend(bbox_to_anchor=(0.8, 1.25))
+ax.legend(bbox_to_anchor=(0.8, 1.05))
 
+pyplot.draw()
 
-fig2 = pyplot.figure("Polymerplot")
+fig2 = pyplot.figure("Gyradius")
 ax2 = fig2.add_subplot(111)
 ax2.set_xlim(1,np.max(lengths))
 
 ax2.errorbar(lengths, avrGyradius,yerr=errGyradius**(1/2),fmt='ro', label="Gyradius")
-
+ax2.plot(lengths, theo2, 'g--', label="Fitted Gyradius")
 ax2.legend(bbox_to_anchor=(0.8, 1.05))
+
+pyplot.draw()
+
+pyplot.ion()
 pyplot.show()
+raw_input('Press <ENTER> to continue')
